dd4b45623f81b7c7636c8becd115ae7d4b2f2fe9
maximlt <mliquet@anaconda.com>
2025-05-10 16:20:02 +0200

    clean up conf.py

--- tutorial/conf.py
+++ tutorial/conf.py
@@ -1,31 +1,30 @@
 # -*- coding: utf-8 -*-
-import subprocess
+from datetime import date
 
 from nbsite.shared_conf import *
 
 
-project = "HoloViz"
+project = "HoloViz Tutorial"
 authors = 'HoloViz authors'
 copyright_years['start_year'] = '2017'
 copyright = copyright_fmt.format(**copyright_years)
-description = 'High-level tools to simplify visualization in Python.'
+description = 'The HoloViz Tutorial.'
 
 # version = release  = base_version(ret.stdout.strip()[1:])
-version = release = "0.0.1"
+version = release = date.today().strftime("%Y.%m.%d")
 
 extensions += [
     'nbsite.analytics',
 ]
 
-html_static_path += ['_static', 'assets']
-templates_path += ['_templates']
+html_static_path += ['_static']
 
 html_theme = "pydata_sphinx_theme"
 html_logo = '_static/holoviz-logo-unstacked.svg'
 html_favicon = "_static/favicon.ico"
 
 html_theme_options.update({
-    "github_url": "https://github.com/holoviz/holoviz",
+    "github_url": "https://github.com/holoviz/tutorial",
     "icon_links": [
         {
             "name": "Twitter",
@@ -38,7 +37,6 @@ html_theme_options.update({
             "icon": "fab fa-discourse",
         },
     ],
-    "header_links_before_dropdown": 6,
     "secondary_sidebar_items": [
         "github-stars-button",
         "page-toc",
@@ -49,25 +47,12 @@ html_context.update({
     # Used to add binder links to the latest released tag.
     'last_release': f'v{release}',
     'github_user': 'holoviz',
-    'github_repo': 'holoviz',
+    'github_repo': 'tutorial',
 })
 
 # Uncomment to turn off notebook execution.
-# nb_execution_mode = "off"
+nb_execution_mode = "off"
 
 nbsite_analytics = {
     'goatcounter_holoviz': True,
 }
-
-# Customize as its default is for all the other HoloViz sites.
-nbsite_hv_sidebar_dropdown['dropdown_value'] = {'text': 'HoloViz Sites'}
-del nbsite_hv_sidebar_dropdown['others']
-
-
-# myst_enable_extensions = [
-#     # MySt-Parser will attempt to convert any isolated img tags (i.e. not
-#     # wrapped in any other HTML) to the internal representation used in sphinx.
-#     'html_image',
-# ]
-
-nb_execution_mode = "off"
